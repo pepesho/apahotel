5a6aec412f46d6fb970a08b39b325f94c3e6e0bc
sho ikeda <83848177+pepesho@users.noreply.github.com>
2021-05-20 17:01:34 +0900

    シーだーが実行できる

--- blog/app/Ledger.php
+++ blog/app/Ledger.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Ledger extends Model
 {
     protected $fillable = ['ISBN_id', 'arrival_day'];
+    public $timestamps = false;
     public function catalog()
     {
         return $this->belongsTo(Catalog::class);
