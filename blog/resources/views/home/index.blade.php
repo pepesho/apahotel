d221a9302b7245bc5467a186610c359b16d60360
Hayashi0403 <t.hayashi.php2021@gmail.com>
2021-05-20 10:56:21 +0900

    貸出ページ

--- blog/resources/views/home/index.blade.php
+++ blog/resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
     <a href="{{ route('users.index') }}" class="menu_button">職員一覧</a>
     <a href="#" class="menu_button">会員一覧</a>
     <a href="#" class="menu_button">書籍一覧</a>
-    <a href="#" class="menu_button">貸出・返却</a>
+    <a href="{{ route('borrows.index') }}" class="menu_button">貸出・返却</a>
     <a href="#" class="menu_button">延滞者リスト</a>
 </div>
 
