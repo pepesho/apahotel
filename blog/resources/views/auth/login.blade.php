fa7eaf1d2b27435b6f3ca55e88708379735e2a97
YamamotoRina <l.yamamoto.php2021@gmail.com>
2021-05-24 13:57:08 +0900

    css変更２

--- blog/resources/views/auth/login.blade.php
+++ blog/resources/views/auth/login.blade.php
@@ -1,24 +1,37 @@
-@extends('layouts.app')
-
-@section('content')
-<h1>ログイン</h1>
-@include('commons/flash')
-<form action="{{ route('login') }}" method="post">
-    @csrf
-    <p>
-        <label>メールアドレス</label><br>
-        <input type="email" name="email" value="{{ old('email') }}">
-    </p>
-    <p>
-        <label class="visually-hidden">パスワード</label><br>
-        <input type="password" name="password" value="">
-    </p>
-    <p>
-        <button type="submit" class="btn btn-primary">ログイン</button>
-    </p>
-    <p>または</p>
-    <p>
-        <a href="{{ route('register') }}">職員登録</a>
-    </p>
-</form>
-@endsection
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>{{ config('app.name') }}</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/main.css">
+        <script src="https://kit.fontawesome.com/fd663eae39.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <header>
+            <i class="fas fa-book"></i><span class="nav_top_text">図書管理システム</span>
+        </header>
+        <main>
+            <h1>ログイン</h1>
+            @include('commons/flash')
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <p>
+                    <label>メールアドレス</label><br>
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </p>
+                <p>
+                    <label class="visually-hidden">パスワード</label><br>
+                    <input type="password" name="password" value="">
+                </p>
+                <p>
+                    <button type="submit" class="btn btn-primary">ログイン</button>
+                </p>
+                <p>または</p>
+                <p>
+                    <a href="{{ route('register') }}">職員登録</a>
+                </p>
+            </form>
+        </main>
+    </body>
+</html>
--- blog/resources/views/home/index.blade.php
+++ blog/resources/views/home/index.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="menu_wrapper container">
-    <div class="button"><a href="{{ route('users.index') }}" class="menu_button">職員一覧</a>
-    <a href="{{ route('members.index') }}" class="menu_button">会員一覧</a></div>
-    <div class="button"><a href="{{ route('catalogs.index') }}" class="menu_button">登録書籍</a>
-    <a href="{{ route('ledgers.index') }}" class="menu_button">館内書籍一覧</a></div>
-    <div class="button"><a href="{{ route('borrows.index') }}" class="menu_button">貸出・返却</a>
-    <a href="#" class="menu_button">延滞者リスト</a></div>
+<div class="container">
+    <div class="menu_wrapper">
+        <a href="{{ route('users.index') }}" class="menu_button">職員一覧</a>
+        <a href="{{ route('members.index') }}" class="menu_button">会員一覧</a>
+        <a href="{{ route('catalogs.index') }}" class="menu_button">登録書籍</a>
+        <a href="{{ route('ledgers.index') }}" class="menu_button">館内書籍一覧</a>
+        <a href="{{ route('borrows.index') }}" class="menu_button">貸出・返却</a>
+        <a href="#" class="menu_button">延滞者リスト</a>
+    </div>
 </div>
 
 @endsection
