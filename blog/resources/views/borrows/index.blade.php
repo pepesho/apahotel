5e4861a2a132bf5bf33dcaf7dc4fa140dc81fc9c
sho ikeda <83848177+pepesho@users.noreply.github.com>
2021-05-25 09:44:40 +0900

    貸出ビューの変更

--- blog/resources/views/borrows/index.blade.php
+++ blog/resources/views/borrows/index.blade.php
@@ -1,29 +1,28 @@
 @extends('layouts.app')
-
 @section('content')
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        {{-- 会員ID検索 --}}
-        @include('commons/flash')
+    {{-- 会員ID検索 --}}
+    @include('commons/flash')
     <div id="search">
-            <h1>会員ID検索</h1>
-            <form action="{{ route('borrows.index') }}" method="post" >
-                @csrf
-                @method('get')          
-                <input type="text" name="member_id" value="{{ old('member_id')}}">
-                <button type="submit" id="query">照会</button>
-            </form>
-    </div>
-    <div id="borrow_form">
-        <h1>貸出</h1>
-        <form action="{{ route('borrows.store') }}" method="post">
+        <h1>会員ID検索</h1>
+        <form action="{{ route('borrows.index') }}" method="post">
             @csrf
-            <p>本のID<input type="text" name="ledger_id"></p>
-            <p>会員ID<input type="text" name="member_id" value=""></p>
-            <button  id="borrow">貸出</button>
+            @method('get')
+            <input type="text" name="member_id" value="{{ old('member_id') }}">
+            <button type="submit" id="query">照会</button>
         </form>
-    
-        <h1>返却</h1>
-        <table border="1">
+    </div>
+    <p>{{ session('error') }}</p>
+    @if ($search_flag)
+        <div id="borrow_form">
+            <h1>貸出</h1>
+            <form action="{{ route('borrows.store') }}" method="post">
+                @csrf
+                <p>本のID<input type="text" name="ledger_id"></p>
+                <p>会員ID<input type="text" name="member_id" value=""></p>
+                <button id="borrow">貸出</button>
+            </form>
+            <h1>返却</h1>
+            <table border="1">
                 <tr>
                     <th>書籍名</th>
                     <th>会員名</th>
@@ -32,39 +31,20 @@
                     <th>返却</th>
                 </tr>
                 @foreach ($borrows as $borrow)
-                    <form action="{{ route('borrows.destroy',$borrow->id) }}" method="post">
-                @csrf
-                @method('delete')
-                <tr>
-                    <td>{{ $borrow->ledger->catalog->title }}</td>
-                    <td>{{ $borrow->member->name }}</td>
-                    <td>{{ $borrow->borrow_date }}</td>
-                    <td>{{ $borrow->return_date }}</td>
-                    <td><input type="checkbox" name="checked[]"  value="{{$borrow->id}}"></td>
-                </tr>
+                    <form action="{{ route('borrows.destroy', $borrow->id) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <tr>
+                            <td>{{ $borrow->ledger->catalog->title }}</td>
+                            <td>{{ $borrow->member->name }}</td>
+                            <td>{{ $borrow->borrow_date }}</td>
+                            <td>{{ $borrow->return_date }}</td>
+                            <td><input type="checkbox" name="checked[]" value="{{ $borrow->id }}"></td>
+                        </tr>
                 @endforeach
-        </table>
-                    <button type="submit" id="return">返却</button>
-                    </form>   
+            </table>
+            <button type="submit" id="return">返却</button>
+            </form>
+    @endif
     </div>
-    {{-- <script>
-        $("#borrow_form").hide();
-        //照会ボタンをクリックしたときの挙動
-        $("#query").click(function(e){ 
-            $("#form1").submit();  
-            e.preventDefault();       
-            $("#search").hide();
-            $("#borrow_form").show();
-
-        });
-        // //貸出ボタンを押したとき非表示
-        // $("#borrow").click(function(){
-        //     $("borrow_form").hide();
-        // });
-        // $("#return").click(function(){
-        //     $("borrow_form").hide();
-        // });
-    </script>          
-         --}}
-    {{-- 会員IDが検索されたら表示する --}}
 @endsection
