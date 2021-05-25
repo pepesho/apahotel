5b094d02f7e576d7095fdde862cfad47c0505f99
YamamotoRina <l.yamamoto.php2021@gmail.com>
2021-05-25 16:54:04 +0900

    cssちょっと変更

--- blog/resources/views/books/create.blade.php
+++ blog/resources/views/books/create.blade.php
@@ -7,16 +7,15 @@
     @csrf
     <dl>
         <dt>ISBN番号</dt>
-        <dd><input type="text" name="ISBN_id"></dd>
+        <dd><input type="text" name="ISBN_id" class="form_input"></dd>
         <dt> タイトル</dt>
-        <dd><input type="text" name="title"></dd>
+        <dd><input type="text" name="title" class="form_input"></dd>
         <dt>著者</dt>
-        <dd><input type="text" name="author"></dd>
+        <dd><input type="text" name="author" class="form_input"></dd>
         <dt>ジャンル</dt>
         <dd>
-        <select name="genre_id" id="sbox">
-             <option value='' disabled selected style='display:none;'>ジャンル選択</option>
-             <option value=""></option>
+        <select name="genre_id" id="sbox" class="form_input">
+             <option value="" disabled selected style='display:none;'>ジャンル選択</option>
         @foreach($genres as $genre)
              <option value="{{$genre->id}}"{{request('genre_id')==$genre->id?'selected':''}}>
                  {{$genre->id}} {{$genre->genre}}
@@ -25,11 +24,11 @@
         </select>
         </dd>
         <dt>出版社</dt>
-        <dd><input type="text" name="publisher"></dd>
+        <dd><input type="text" name="publisher" class="form_input"></dd>
         <dt>出版日</dt>
-        <dd><input type="date" name="publisher_date"></dd>
+        <dd><input type="date" name="publisher_date" class="form_input"></dd>
     </dl>
-    <button type="submit">登録する</button>
+    <button type="submit" class="btn btn-primary">登録する</button>
 </form>
 <a href="{{ route('catalogs.index') }}">一覧ページに戻る</a>
 @endsection
